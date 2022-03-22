dd086d4aa0e6f4a82dac7766db1e74557e4ab7e6
FaresMulla <farismolla888@gmail.com>
2022-03-22 21:24:14 +0200

    fonts

--- src/components/app-header.vue
+++ src/components/app-header.vue
@@ -3,22 +3,22 @@
 <section class="navBar col-10 m-auto mb-5">
      
     <div class="d-flex bd-highlight">       
-      <div class="p-1 flex-grow-1 bd-highlight">
-          <router-link to="/"> <button class="btn btn-outline-light text-dark">Home</button></router-link>
+      <div class="p-1 flex-grow-1 bd-highlight align-self-center">
+          <router-link to="/"> <button class="btn btn-outline-light text-dark fs-3" style="font-weight: bold; font-family: 'Crimson Pro', serif;">Fastlancer<span class="text-success">.</span> </button></router-link>
       </div>
-      <div class="p-1 bd-highlight">
+      <div class="p-1 bd-highlight align-self-center">
           <router-link to="/gig"> <button class="btn btn-outline-light text-dark float-end" >Explore</button></router-link>
       </div>
-      <div class="p-1 bd-highlight">
+      <div class="p-1 bd-highlight align-self-center">
           <router-link to="/"> <button class="btn btn-outline-light text-dark float-end" disabled>Become a Seller</button></router-link>
       </div>
-      <div class="p-1 bd-highlight">
+      <div class="p-1 bd-highlight align-self-center">
           <router-link to="/"> <button class="btn btn-outline-light text-dark float-end" disabled>Sign In</button></router-link>
       </div>
-      <div class="p-1 bd-highlight">
+      <div class="p-1 bd-highlight align-self-center">
           <router-link to="/"> <button class="btn btn-outline-light text-dark float-end" disabled>Join</button></router-link>
       </div>
-      <div class="p-1 bd-highlight">
+      <div class="p-1 bd-highlight align-self-center">
           <router-link to="/about"> <button class="btn btn-outline-light text-dark float-end">About</button></router-link>
       </div>
     </div>
@@ -27,7 +27,7 @@
     <section class="search-home col-10 m-auto mt-5">
         <div class="col-6">
             <p class="lead fs-3">
-                Find the perfect <span class="text-danger">freelance</span> <br>
+                Find the perfect <span class="text-danger" style="font-family: 'Inspiration', cursive;">freelance</span> <br>
                 services for your business
             </p>
             <div class="input-group mb-5">              
