e7974810b4f781f21558f629686e7e120d5fec73
Yonatan-shitrit <jonatan.shitrit@gmail.com>
2022-03-23 14:05:42 +0200

    carousel 2

--- src/components/category-carousel.vue
+++ src/components/category-carousel.vue
@@ -1,8 +1,19 @@
 <template>
-
+<div>
+    <VueSlickCarousel >
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <div>4</div>
+    </VueSlickCarousel>
+  </div>
 </template>
 
 <script>
+//   import VueSlickCarousel from 'vue-slick-carousel'
+  import 'vue-slick-carousel/dist/vue-slick-carousel.css'
+  // optional style for arrows & dots
+  import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
 export default {
 
 }
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,11 +1,17 @@
 <template>
 <section class="col-10 m-auto">
   <h1>Home page</h1>
+  <category-carousel :arrows="true" :dots="true" />
   </section>
 </template>
 
 <script>
+import categoryCarousel from "../components/category-carousel.vue"
+
 export default {
+    components:{
+    categoryCarousel,
+}
 
 }
 </script>
