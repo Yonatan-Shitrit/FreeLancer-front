3c33830fd36b6037758a32624bce08b6999ef479
Yonatan-shitrit <jonatan.shitrit@gmail.com>
2022-03-23 17:22:21 +0200

    stage 3

--- src/views/home.vue
+++ src/views/home.vue
@@ -2,16 +2,24 @@
 <section class="col-10 m-auto">
   <h1>Home page</h1>
   <category-carousel :arrows="true" :dots="true" />
+        <div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <div>4</div>
+  <category-carousel/>
   </section>
 </template>
 
 <script>
+  // import 'vue-slick-carousel/dist/slick.css'
+  // import 'vue-slick-carousel/dist/slick-theme.css'
 import categoryCarousel from "../components/category-carousel.vue"
+
 
 export default {
     components:{
-    categoryCarousel,
-}
+    categoryCarousel
+},
 
 }
 </script>
