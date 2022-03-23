b72b860ff86b05acd15611958fa15c38ea9bd6fd
Jonathan <yonatan5789@gmail.com>
2022-03-23 19:36:12 +0200

    yes

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,64 +1,57 @@
 <template>
-<section class="col-10 m-auto">
-  <h1>Home page</h1>
-  <category-carousel :arrows="true" :dots="true" />
-        <div>1</div>
-      <div>2</div>
-      <div>3</div>
-      <div>4</div>
-  <category-carousel/>
-  </section>
-<section class="gig-home" >
-<section class="gig-home-top" >
-    <datalist id="categories">
-          <option value="logo maker"/>
-          <option value="build app"/>
-          <option value="build web"/>
-          <option value="create video"/>
-          <option value="create gallery"/>
-        </datalist>
-        
-        <div class="left-side">
-            <div class="sub-left-side1">
-            <p class="mini-dis">
-                Find the perfect <span class="text-success" style="font-weight: bold; font-family: 'domaine', cursive;">freelance</span> <br>
-                services for your business
-            </p>
-            </div>
-            <div class="sub-left-side2">              
-              <span><button class="search-icon"><i class="bi bi-search"></i></button></span>
-              <input list="categories" type="text" class="search-input" :placeholder="searchPlaceHolder">
-              <span ><button class="search-btn"> search</button></span>
-            </div>          
-            
-        </div>
-        
-    
+  <section class="gig-home">
+    <section class="gig-home-top">
+      <datalist id="categories">
+        <option value="logo maker" />
+        <option value="build app" />
+        <option value="build web" />
+        <option value="create video" />
+        <option value="create gallery" />
+      </datalist>
 
-</section>
-</section>
-  
+      <div class="left-side">
+        <div class="sub-left-side1">
+          <p class="mini-dis">
+            Find the perfect
+            <span
+              class="text-success"
+              style="font-weight: bold; font-family: 'domaine', cursive"
+              >freelance</span
+            >
+            <br />
+            services for your business
+          </p>
+        </div>
+        <div class="sub-left-side2">
+          <span
+            ><button class="search-icon"><i class="bi bi-search"></i></button
+          ></span>
+          <input
+            list="categories"
+            type="text"
+            class="search-input"
+            :placeholder="searchPlaceHolder"
+          />
+          <span><button class="search-btn">search</button></span>
+        </div>
+      </div>
+    </section>
+  </section>
 </template>
 
 <script>
-  // import 'vue-slick-carousel/dist/slick.css'
-  // import 'vue-slick-carousel/dist/slick-theme.css'
-import categoryCarousel from "../components/category-carousel.vue"
-
 
 export default {
-    computed:{
-        searchPlaceHolder(){
-            return `Try "building mobile app"`
-        }
+  computed: {
+    searchPlaceHolder() {
+      return `Try "building mobile app"`;
     },
-    components:{
-    categoryCarousel
-},
+  },
+  components: {
 
-}
+  },
+};
 </script>
 
 <style>
-
 </style>
