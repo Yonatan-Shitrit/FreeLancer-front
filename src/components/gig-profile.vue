456a543e099d5556718fd21819bec3d0bdb8bf6a
Jonathan <yonatan5789@gmail.com>
2022-03-24 18:30:19 +0200

    ihye beseder

---
+++ src/components/gig-profile.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="profile-card">
+    <div class="profile-info">
+      <img src="https://fiverr-res.cloudinary.com/t_profile_original,q_auto,f_auto/attachments/profile/photo/5344c10fd4820db3626c4fc24968783d-1588608774469/1e4a3bd9-b71d-48ce-8ac0-0ff6d667caf4.jpeg" alt="" />
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
