3abd637b3002f306d1738b3dbcb9fa733f55dc63
Yonatan-shitrit <jonatan.shitrit@gmail.com>
2022-03-25 16:07:57 +0300

    carousel 5

--- src/components/main-carousel.vue
+++ src/components/main-carousel.vue
@@ -20,13 +20,10 @@
   text-align:left;
   font-weight: 700;
   ">
-   <small style="font-size: 14px; font-weight: 100;">{{category.name}}</small><br>
+   <small style="font-size: 14px; font-weight: 100;">{{category.description}}</small><br>
   {{category.name}}
   </h4>
-
     </Slide>
-    
-    
     <template #addons>
       <Navigation />
       <Pagination />
@@ -40,24 +37,17 @@ import { Carousel, Slide, Pagination, Navigation } from "vue3-carousel";
 export default {
     data(){
         return{
-        slides:[
-            {
-                imgSrc:"",
-                slideSubHeader:"",
-                slideHeader:"",
-                
-            }],
-        categories: [
-            {name: 'Logo Design', image:'https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741678/logo-design-2x.png'},
-            {name: 'Word Press', image:'https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/ae11e2d45410b0eded7fba0e46b09dbd-1598561917003/wordpress-2x.png'},
-            {name: 'Voice Over', image:'https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741669/voiceover-2x.png'},
-            {name: 'Social Media', image:'https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741663/animated-explainer-2x.png'},
-            {name: 'Translation', image:'https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741667/social-2x.png'},
-            {name: 'Logo Design', image:'https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741678/logo-design-2x.png'},
-            {name: 'Word Press', image:'https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/ae11e2d45410b0eded7fba0e46b09dbd-1598561917003/wordpress-2x.png'},
-            {name: 'Voice Over', image:'https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741669/voiceover-2x.png'},
-            {name: 'Social Media', image:'https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741663/animated-explainer-2x.png'},
-            {name: 'Translation', image:'https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741667/social-2x.png'}
+            categories: [
+            {name: 'Logo Design', description: "Build your brand", image:'https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741678/logo-design-2x.png'},
+            {name: 'Word Press', description: "Costumize your site", image:'https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/ae11e2d45410b0eded7fba0e46b09dbd-1598561917003/wordpress-2x.png'},
+            {name: 'Voice Over', description: "Share your message", image:'https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741669/voiceover-2x.png'},
+            {name: 'Video Explainer', description: "Engage your audience", image:'https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741663/animated-explainer-2x.png'},
+            {name: 'Social Media', description: "Reach more customers", image:'https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741667/social-2x.png'},
+            {name: 'SEO', description: "Unlock growth online", image:'https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741678/logo-design-2x.png'},
+            {name: 'Illustration', description: "Color your dreams", image:'https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/ae11e2d45410b0eded7fba0e46b09dbd-1598561917003/wordpress-2x.png'},
+            {name: 'Translation', description: "Go global", image:'https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741669/voiceover-2x.png'},
+            {name: 'Data Entry', description: "Learn your business", image:'https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741663/animated-explainer-2x.png'},
+            {name: 'Book Covers', description: "Showcase your story", image:'https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741667/social-2x.png'}
           ]
     }},
     methods:{
--- src/views/home.vue
+++ src/views/home.vue
@@ -35,7 +35,7 @@
   </section>
   <section class="main-home-1">  
   <h2 style="font-size: 32px; line-height: 120%; color: #404145; font-weight: 700;"> Popular professional services </h2>
-    <main-carousel   style=" margin:auto; width:100%"  :itemsToShow="5" :itemsToScroll="5"  :wrapAround="true" :transition="400">
+    <main-carousel   style=" margin:auto; width:calc (100% + 20px);"  :itemsToShow="5" :itemsToScroll="5"  :modelValue="2" :wrapAround="true" :transition="400">
   </main-carousel>  
   <!-- <pre><h2>Our Gigs:</h2>
   {{gigs}}</pre> -->
