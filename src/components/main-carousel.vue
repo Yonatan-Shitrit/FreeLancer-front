6583f15e04cbe5bd9e411e8cc73a33fea8b7c737
Yonatan-shitrit <jonatan.shitrit@gmail.com>
2022-03-25 14:58:20 +0300

    carousel spinning

--- src/components/main-carousel.vue
+++ src/components/main-carousel.vue
@@ -1,12 +1,13 @@
 <template>
 <!-- <pre>Categories: {{categories}}</pre> -->
     <Carousel>
-    <Slide v-for="category in 1" :key="category" >
-      <img style=" border-radius: 4px; margin:18px; width:252px; height: 345px"  src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741678/logo-design-2x.png">
+    <Slide  style="padding: 0 10px 0 10px;" v-for="category in this.categories" :key="category" >
+      <img v-bind:src=" category.image"/>
+      <!-- <img style=" border-radius: 4px; margin:18px; width:252px; height: 345px"  src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741678/logo-design-2x.png">
       <img style=" border-radius: 4px; margin:18px; width:252px; height: 345px"  src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/ae11e2d45410b0eded7fba0e46b09dbd-1598561917003/wordpress-2x.png">
       <img style=" border-radius: 4px; margin:18px; width:252px; height: 345px"  src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741669/voiceover-2x.png">
       <img style=" border-radius: 4px; margin:18px; width:252px; height: 345px"  src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741663/animated-explainer-2x.png">
-      <img style=" border-radius: 4px; margin:18px; width:252px; height: 345px"  src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741667/social-2x.png">
+      <img style=" border-radius: 4px; margin:18px; width:252px; height: 345px"  src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741667/social-2x.png"> -->
     </Slide>
     
     
@@ -31,6 +32,11 @@ export default {
                 
             }],
         categories: [
+            {name: 'Logo Design', image:'https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741678/logo-design-2x.png'},
+            {name: 'Word Press', image:'https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/ae11e2d45410b0eded7fba0e46b09dbd-1598561917003/wordpress-2x.png'},
+            {name: 'Voice Over', image:'https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741669/voiceover-2x.png'},
+            {name: 'Social Media', image:'https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741663/animated-explainer-2x.png'},
+            {name: 'Translation', image:'https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741667/social-2x.png'},
             {name: 'Logo Design', image:'https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741678/logo-design-2x.png'},
             {name: 'Word Press', image:'https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/ae11e2d45410b0eded7fba0e46b09dbd-1598561917003/wordpress-2x.png'},
             {name: 'Voice Over', image:'https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741669/voiceover-2x.png'},
