64a792c17859509994c22aedf3630eedc900c566
Jonathan <yonatan5789@gmail.com>
2022-03-26 20:59:20 +0300

    hh

--- src/components/app-header.vue
+++ src/components/app-header.vue
@@ -1,30 +1,40 @@
 <template>
-<section class="full-header">
-<section class="app-header">
-    <ul>
-      <li class="header-item logo"> <router-link to="/"><a href="">Fastlancer<span>.</span></a> </router-link></li>
-      <li class="header-item"> <router-link to="/"><a class="not-exist" href="">Join</a> </router-link></li>      
-      <li class="header-item"> <router-link to="/login"><a class="" href="">Sign In</a> </router-link></li>
-      <li class="header-item"> <router-link to="/"><a class="not-exist" href="">Become a Seller</a> </router-link></li>
-      <li class="header-item"> <router-link to="/gig"><a href="">Explore</a> </router-link></li>
-    </ul>
-
-</section> 
-</section>
- 
+  <section class="full-header">
+    <section class="app-header">
+      <router-link class="logo" to="/">Fastlancer<span>.</span></router-link>
+      <ul>
+        <li class="header-item">
+          <router-link to="/"
+            ><a class="not-exist" href="">Join</a>
+          </router-link>
+        </li>
+        <li class="header-item">
+          <router-link to="/login"
+            ><a class="" href="">Sign In</a>
+          </router-link>
+        </li>
+        <li class="header-item">
+          <router-link to="/"
+            ><a class="not-exist" href="">Become a Seller</a>
+          </router-link>
+        </li>
+        <li class="header-item">
+          <router-link to="/gig"><a href="">Explore</a> </router-link>
+        </li>
+      </ul>
+    </section>
+  </section>
 </template>
 
 <script>
 export default {
-    
-    computed:{
-        searchPlaceHolder(){
-            return `🔍 Try "building mobile app"`
-        }
-    }
-}
+  computed: {
+    searchPlaceHolder() {
+      return `🔍 Try "building mobile app"`;
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
