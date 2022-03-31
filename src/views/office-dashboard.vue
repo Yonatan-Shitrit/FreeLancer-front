7ad7104f6a094752b406701ceb795cdc6ac07ccb
Jonathan <yonatan5789@gmail.com>
2022-03-31 20:23:10 +0300

    gg

--- src/views/office-dashboard.vue
+++ src/views/office-dashboard.vue
@@ -24,7 +24,7 @@
         </div>
         <div class="orders-container">
           <ul>
-           <div class="order-categories">
+            <div class="order-categories">
               <div class="category-title">Gig</div>
               <div class="category-title">Gig</div>
               <div class="category-name">Buyer</div>
@@ -32,13 +32,17 @@
               <div class="category-actions-dashboard">Actions</div>
               <div class="category-status">status</div>
             </div>
-            <li  v-for="order in orders" :key="order._id">
+            <li v-for="order in orders" :key="order._id">
               <img :src="getGig(order.gigId).images[0]" alt="" />
-              <div class="gig-title">{{getGig(order.gigId).title}}</div>
-              <div class="buyer-name">{{getGig(order.gigId).seller.fullName}}</div>
+              <div class="gig-title">{{ getGig(order.gigId).title }}</div>
+              <div class="buyer-name">
+                {{ getGig(order.gigId).seller.fullName }}
+              </div>
               <div class="price">${{ getGig(order.gigId).price }}</div>
-              <div class="actions"><span>Cancel </span>&nbsp;<span> Accept</span></div>
-              <div class="status-dashboard">{{order.status}}</div>
+              <div class="actions">
+                <span @click="changeStatus(order,'canceled')">Cancel </span>&nbsp;<span @click="changeStatus(order,'active')"> Accept</span>
+              </div>
+              <div class="status-dashboard">{{ order.status }}</div>
             </li>
           </ul>
         </div>
@@ -52,10 +56,21 @@ export default {
   data() {
     return {};
   },
-   methods: {
+  methods: {
     getGig(id) {
       const gigs = this.$store.getters.gigs;
       return gigs.filter((gig) => gig._id === id)[0];
+    },
+    async changeStatus(order, status) {
+      order = JSON.parse(JSON.stringify(order));
+      order.status = status;
+      order.updatedAt = Date.now();
+      console.log("updateOrder", order);
+      try{
+        await this.$store.dispatch({ type: "saveOrder", order });
+      }catch{
+        console.log('unable to change order status', err);
+      }
     },
   },
   computed: {
@@ -63,7 +78,7 @@ export default {
       console.log("logged user", this.$store.getters.user);
       return this.$store.getters.user;
     },
-     orders() {
+    orders() {
       const orders = this.$store.getters.orders;
       console.log("orders", orders);
       return orders.filter((order) => order.sellerId === this.loggedinUser._id);
