b788f2023b64d5fe8e0f536c4bd53c1ac6f5dcd8
Jonathan <yonatan5789@gmail.com>
2022-04-04 00:50:22 +0300

    ggg

--- src/views/office-dashboard.vue
+++ src/views/office-dashboard.vue
@@ -14,7 +14,7 @@
               <h6>Inbox response rate</h6>
               <div class="proggress-container">
                 <div class="progress-bar">
-                  <span :style="{ width: '100%' }"></span>
+                  <span :style="{ width: '87%' }"></span>
                 </div>
                 <div class="percentage-text">87%</div>
               </div>
@@ -23,7 +23,7 @@
               <h6>Inbox response time</h6>
               <div class="proggress-container">
                 <div class="progress-bar">
-                  <span :style="{ width: '100%' }"></span>
+                  <span :style="{ width: '91%' }"></span>
                 </div>
                 <div class="percentage-text">91%</div>
               </div>
@@ -32,7 +32,7 @@
               <h6>Order response rate</h6>
               <div class="proggress-container">
                 <div class="progress-bar">
-                  <span :style="{ width: '100%' }"></span>
+                  <span :style="{ width: '67%' }"></span>
                 </div>
                 <div class="percentage-text">67%</div>
               </div>
@@ -41,7 +41,7 @@
               <h6>Delivered on time</h6>
               <div class="proggress-container">
                 <div class="progress-bar">
-                  <span :style="{ width: '100%' }"></span>
+                  <span :style="{ width: '98%' }"></span>
                 </div>
                 <div class="percentage-text">98%</div>
               </div>
@@ -50,16 +50,16 @@
               <h6>Order completion</h6>
               <div class="proggress-container">
                 <div class="progress-bar">
-                  <span :style="{ width: completedPercentage+'%' }"></span>
+                  <span :style="{ width: completedPercentage + '%' }"></span>
                 </div>
-                <div class="percentage-text">{{completedPercentage}}%</div>
+                <div class="percentage-text">{{ completedPercentage }}%</div>
               </div>
             </li>
           </ul>
         </div>
         <div class="seller-earnings">
           <div>Earned Last Month</div>
-          <span>${{totalEarned.toLocaleString("en-US")}}</span>
+          <span>${{ totalEarned.toLocaleString("en-US") }}</span>
         </div>
       </div>
       <div class="dashboard-orders">
@@ -91,22 +91,43 @@
               <div class="category-status">status</div>
             </div>
             <li v-for="order in orders" :key="order._id">
-              <img :src="getGig(order.gigId).images[0]" alt="" />
-              <div class="gig-title">{{ getGig(order.gigId).title }}</div>
-                            <div class="buyer-date"></div> 
-              <div class="buyer-name">
-                {{ getGig(order.gigId).seller.fullName }}
+              <div class="media-q">
+                <img :src="getGig(order.gigId).images[0]" alt="" />
+                <div class="gig-title">{{ getGig(order.gigId).title }}</div>
+                <div class="buyer-date"></div>
+                <div class="buyer-name">
+                  {{ getGig(order.gigId).seller.fullName }}
+                </div>
+                <div class="price">
+                  ${{ getGig(order.gigId).price.toLocaleString("en-US") }}
+                </div>
+                <div class="actions">
+                  <span @click="changeStatus(order, 'canceled')">Cancel </span
+                  >&nbsp;<span @click="changeStatus(order, 'active')">
+                    Accept</span
+                  >
+                </div>
+                <div class="status-dashboard">{{ order.status }}</div>
               </div>
-              <div class="price">
-                ${{ getGig(order.gigId).price.toLocaleString("en-US") }}
+
+              <div class="mobile-card">
+                <div class="mobile-title">{{ getGig(order.gigId).title }}</div>
+                <div class="mobile-name">
+                  <span>Buyer: {{ getGig(order.gigId).seller.fullName }}</span>
+                  <span
+                    >Price: ${{
+                      getGig(order.gigId).price.toLocaleString("en-US")
+                    }}</span
+                  >
+                </div>
+                <div class="mobile-status">
+                  <span>Status: {{ order.status }}</span>
+                  <span>Date: 16456</span>
+                </div>
+                <div class="mobile-actions">
+                  <span class="cancel" @click="changeStatus(order, 'canceled')">Cancel </span>&nbsp;<span class="accept" @click="changeStatus(order, 'active')">Accept</span>
+                </div>
               </div>
-              <div class="actions">
-                <span @click="changeStatus(order, 'canceled')">Cancel </span
-                >&nbsp;<span @click="changeStatus(order, 'active')">
-                  Accept</span
-                >
-              </div>
-              <div class="status-dashboard">{{ order.status }}</div>
             </li>
           </ul>
         </div>
@@ -151,7 +172,11 @@ export default {
     },
     completedOrders() {
       const orders = this.$store.getters.orders;
-      return orders.filter((order) => order.sellerId === this.loggedinUser._id && order.status === 'completed');
+      return orders.filter(
+        (order) =>
+          order.sellerId === this.loggedinUser._id &&
+          order.status === "completed"
+      );
     },
     allOrders() {
       const orders = this.$store.getters.orders;
@@ -175,9 +200,9 @@ export default {
         return acc + this.getGig(order.gigId).price;
       }, 0);
     },
-    completedPercentage(){
-      return ((this.completedOrders.length*100)/this.allOrders.length)
-    }
+    completedPercentage() {
+      return (this.completedOrders.length * 100) / this.allOrders.length;
+    },
   },
 };
 </script>
