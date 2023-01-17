6fff4469eec6eebc509f408c8d3273dc7e11be6f
Yonatan-shitrit <jonatan.shitrit@gmail.com>
2023-01-17 16:08:54 +0200

    ff

--- src/services/http-service.js
+++ src/services/http-service.js
@@ -1,6 +1,7 @@
 import Axios from 'axios'
 
-const BASE_URL = process.env.NODE_ENV === 'production' ? '/api/' : '//localhost:3030/api/'
+
+const BASE_URL = process.env.NODE_ENV === 'production' ? `${process.env.VUE_APP_SERVER_URL}/api/` : '//localhost:3030/api/'
 
 var axios = Axios.create({
   withCredentials: true,
