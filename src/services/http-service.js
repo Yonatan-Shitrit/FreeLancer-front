62b13ca26dcfa3b421cfa94c0d5d68453870e0c0
Yonatan-shitrit <jonatan.shitrit@gmail.com>
2023-01-18 16:18:37 +0200

    df

--- src/services/http-service.js
+++ src/services/http-service.js
@@ -23,6 +23,7 @@ export const httpService = {
 }
 
 async function ajax(endpoint, method = 'GET', data = null) {
+  console.log('environment base url:', BASE_URL);
   try {
     const res = await axios({
       url: `${BASE_URL}${endpoint}`,
